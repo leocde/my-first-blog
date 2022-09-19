2d321728c594be1dd8771368493b0eae16fe7503
leo <sanz.leonardo90@gmail.com>
2022-09-19 09:11:46 -0300

    Added  view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     
     path('', views.post_list, name='post_list'),
+    # This part post/<int:pk>/ specifies a URL pattern
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+
 ]
